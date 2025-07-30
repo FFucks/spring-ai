1c48cd959555f625a7c6a901504901832f7dbe7e
FFucks <fabio_geiss@hotmail.com>
2025-07-29 22:15:58 -0300

    spring ai config

--- src/main/java/com/ffucks/Application.java
+++ src/main/java/com/ffucks/Application.java
@@ -1,8 +1,12 @@
 package com.ffucks;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello, FFucks Application!");
+        SpringApplication.run(Application.class, args);
     }
 }
