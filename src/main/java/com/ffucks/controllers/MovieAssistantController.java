df723ab54de3bbbdbd5b42c5552b82b4be9d722d
FFucks <fabio_geiss@hotmail.com>
2025-08-31 21:38:52 -0300

    Adjusts on openAi

--- src/main/java/com/ffucks/controllers/MovieAssistantController.java
+++ src/main/java/com/ffucks/controllers/MovieAssistantController.java
@@ -1,7 +1,6 @@
 package com.ffucks.controllers;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
